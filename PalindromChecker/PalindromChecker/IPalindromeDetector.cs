aee3ca5b5270a82721b8985100108cfb79473fbe
Nuno Santos <nunosn@yahoo.com.br>
2013-07-09 23:03:34 +0100

    added palindromic input check

---
+++ PalindromChecker/PalindromChecker/IPalindromeDetector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeChecker
+{
+    interface IPalindromeDetector
+    {
+        bool IsPalindromic(string inputSequence);
+        string GetInput();
+        void DisplayOutput(bool status);
+    }
+}
---
+++ PalindromChecker/PalindromChecker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            IPalindromeDetector obj = new Palindrome();
+            input = obj.GetInput();
+            bool status = obj.IsPalindromic(input);
+            obj.DisplayOutput(status);
+        }
+    }
+}
