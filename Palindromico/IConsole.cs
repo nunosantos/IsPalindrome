a75b6ccd31a20ac941754903d05eb936cbb2afba
Nuno Santos <nunosn@yahoo.com.br>
2013-07-10 22:39:11 +0100

    added unit tests

---
+++ Palindromico/IConsole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palindromico
+{
+    interface IConsole
+    {
+        string GetInput();
+        bool IsPalindromic(string inputSequence);
+        void DisplayOutput(bool status);
+        void WaitInput();
+    }
+}
